ebc12082cf0c3a84853e484382bec90191dd2440
Phuc Lam <phuclamtran2810@gmail.com>
2025-11-28 16:00:34 +0700

    bai 1

---
+++ HelloWord/Program.cs
@@ -0,0 +1,31 @@
+﻿//namespace HelloWord
+namespace MyApp.HelloWord
+{
+    //internal class Program
+    /*
+     * 
+     * 
+     */
+    public class Program
+    {
+        static void Main(string[] args)         
+        {
+            Console.WriteLine("Hello, World!");
+            if (args.Length >0)
+            {
+                foreach(var arg in args)
+
+                {
+                    Console.WriteLine(arg);
+                }      
+            }
+           var s = Console.ReadLine();
+            //int n = int.Parse(s);
+            if (int.TryParse(s, out var val))
+                Console.WriteLine(val);
+            else
+                Console.WriteLine("Invalid int value");
+                Console.WriteLine(val);
+        }
+    }
+}
