808b252aa9afe060b5de14fc57ddcadb29955afb
Phuc Lam <phuclamtran2810@gmail.com>
2025-11-28 17:14:31 +0700

    bai 2

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -1,31 +1,50 @@
-﻿//namespace HelloWord
+﻿
+
 namespace MyApp.HelloWord
 {
-    //internal class Program
-    /*
-     * 
-     * 
-     */
     public class Program
     {
+
+        //int x;
+
         static void Main(string[] args)         
         {
-            Console.WriteLine("Hello, World!");
-            if (args.Length >0)
-            {
-                foreach(var arg in args)
+            //string s = "sadgdfhga \n\r\t dgsd";
+            //string s = @"sadgdfhga \n\r\t dgsd";
+            //string s = $"sadgdfhga \n\r\t dgsd {DateTime.Now} \\";
+            //s = Console.ReadLine();
 
-                {
-                    Console.WriteLine(arg);
-                }      
-            }
-           var s = Console.ReadLine();
-            //int n = int.Parse(s);
-            if (int.TryParse(s, out var val))
-                Console.WriteLine(val);
-            else
-                Console.WriteLine("Invalid int value");
-                Console.WriteLine(val);
+            //StringBuilder stringBuilder = new StringBuilder();
+            //string s1 = $"sadgdfhga \n\r\t dgsd {DateTime.Now} \\";
+            //string s2 = "review";
+            //stringBuilder.Append(s1);
+            //stringBuilder.Append(s2);
+
+            // string s = stringBuilder.ToString();
+            //Console.WriteLine(s);
+
+            //var x = "10";
+
+            //int? x;
+
+            //x = null;
+            //Console.WriteLine(x.HasValue.ToString());
+
+            int n;
+            int max = int.MinValue;
+            Console.Write("So 1: ");
+            n = int.Parse(Console.ReadLine());
+            if (n > max) { max = n; }
+
+            Console.Write("So 2: ");
+            n = int.Parse(Console.ReadLine());
+            if (n > max) { max = n; }
+
+            Console.Write("So 3: ");
+            n = int.Parse(Console.ReadLine());
+            if (n > max) { max = n; }
+
+            Console.WriteLine("So lon nhat la: " + max.ToString());
         }
     }
 }
